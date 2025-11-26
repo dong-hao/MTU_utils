9f1a480ee01b2edc8b0ff3c855bfa483f4dd4662
DONG Hao <donghao@cugb.edu.cn>
2025-11-26 15:11:03 +0800

    Initial commit
    Add files via upload

--- src/readTBL.m
+++ src/readTBL.m
@@ -1,114 +1,218 @@
-function info=readTBL(pname,tblname)
-% read (binary) TBL table file and output info infomation structure
-fid=fopen([pname tblname ],'r','ieee-le');
-ctemp = find_tbltag(fid,'LFRQ',1);
-lfrq = ctemp(13:13);
-find_tbltag(fid,'L3NS',1);
-fseek(fid,-13,'cof');
-l3ns= fread(fid,1,'int');
-find_tbltag(fid,'L4NS',1);
-fseek(fid,-13,'cof');
-l4ns= fread(fid,1,'int');
-ctemp= find_tbltag(fid,'STIM',1);
-stim = datenum([num2str(ctemp(17)),'-',num2str(ctemp(16)),'-','20',...
-    num2str(ctemp(18)),' ',num2str(ctemp(15)),':',num2str(ctemp(14)),':',num2str(ctemp(13))]);
-ctemp = find_tbltag(fid,'ETIM',1);
-etim = datenum([num2str(ctemp(17)),'-',num2str(ctemp(16)),'-','20',...
-    num2str(ctemp(18)),' ',num2str(ctemp(15)),':',num2str(ctemp(14)),':',num2str(ctemp(13))]);
-find_tbltag(fid,'HSMP',1);
+function info=readTBL(fpath,fname)
+% readTBL reads a (binary) TBL table file of the legacy Phoenix format 
+% (MTU-5A) and output the "info" metadata structure
+% fpath: path to the tbl 
+% frame: name of the tbl file (including extensions)
+% info: output struct of the TBL metadata
+% ======================================================================= %
+% definition of the TBL tags (or what I guessed after reading the user 
+% manual and fiddling with their files) 
+% SITE: site name
+% SNUM: serial number (of the box)
+% FILE: file name recorded
+% CMPY: company/institute of the survey
+% SRVY: survey project name
+% EXLN: Ex channel dipole length 
+% EYLN: Ey channel dipole length 
+% NREF: North reference (true, or magnetic north)
+% LNGG: longitude in degree-minute format (DDD MM.MM)
+% LATG: latitude in degree-minute format (DD MM.MM)
+% ELEV: elevation (in metres)
+% HXSN: Hx channel coil serial number
+% HYSN: Hy channel coil serial number
+% HZSN: Hz channel coil serial number
+% STIM: starting time (UTC)
+% ETIM: ending time (UTC)
+% LFRQ: powerline frequency for filtering (can only be 50 or 60 Hz)
+% HGN:  final H-channel gain 
+% HGNC: H-channel gain control: HGN = PA * 2^HGNC (note: PA =
+%       PreAmplifier gain)
+% EGN:  final E-channel gain 
+% EGNC: E-channel gain control: HGN = PA * 2^HGNC (note: PA =
+%       PreAmplifier gain)
+% HSMP: L3 and L4 time slot (in second), this means the instrument will
+%       record L3NS seconds for L3 and L4NS seconds for L4, every HSMP
+%       seconds. 
+% L3NS: L3 sample time (in second)
+% L4NS: L4 sample time (in second)
+% SRL3: L3 sample rate
+% SRL4: L4 sample rate
+% SRL5: L5 sample rate
+% HATT: H channel attenuation (1/4.3 for MTU-5A)
+% HNOM: H channel normalization (mA/nT) 
+% TCMB: Type of comb filter (probably used to suppress the harmonics of the
+%       powerline noise. 
+% TALS: Type of anti-aliasing filter 
+% LPFR: Parameter of Low-pass/VLF filter. this is a quite complicated
+%       part as the low-pass filter is simply an R-C circuit with a switch
+%       to connect to different capacitors. To ensure enough bandwidth
+%       (proportion to 1/RC), one should use smaller capacitors with larger
+%       ground resistance. 
+% ACDC: AC/DC coupling (DC = 0, AC = 1; MT should always be DC)
+% FSCV: full scaling A-D converter voltage (in unit of V)
+% ======================================================================= %
+% note: 
+% Phoenix Legacy TBL is a straight-forward parameter-value metadata file,
+% stored in a bizarre format. The parameter tag and value are stored in a
+% series of 25-byte data blocks, in mixed data type: the first 12 bytes are
+% reserved for the tag name (first 4 bytes as char). The values are stored
+% in the 13 bytes afterwards, in various formats (char, int, float, etc.).
+% 
+% So a good practice is to read in those blocks one by one and extract all 
+% of them. However, not every thing is useful for the metadata, so I only 
+% extract a few of them, for now. 
+% 
+% Hao
+% 2012.07.04
+% Beijing
+% ======================================================================= %
+% firstly open the file
+fid=fopen([fpath fname ],'r','ieee-le');
+% ========================= site basic info  ============================ %
+findTblTag(fid,'SNUM',1);
 fseek(fid,-13,0);
-hsmp = fread(fid,1,'int');
-find_tbltag(fid,'EXLN',1);
+info.SNUM = fread(fid,1,'int');
+ctemp = findTblTag(fid,'SITE',1);
+info.SITE = char(ctemp(13:24));
+ctemp = findTblTag(fid,'FILE',1);
+info.FILE = char(ctemp(13:24));
+ctemp = findTblTag(fid,'CMPY',1);
+info.CMPY = char(ctemp(13:24));
+ctemp = findTblTag(fid,'SRVY',1);
+info.SRVY = char(ctemp(13:24));
+ctemp = findTblTag(fid,'LATG',1);
+info.LATG = char(ctemp(13:24));
+ctemp = findTblTag(fid,'LNGG',1);
+info.LONG = char(ctemp(13:24));
+findTblTag(fid,'ELEV',1);
 fseek(fid,-13,0);
-exln = fread(fid,1,'float64');
-find_tbltag(fid,'EYLN',1);
+info.ELEV = fread(fid,1,'int');
+findTblTag(fid,'NREF',1);
 fseek(fid,-13,0);
-eyln = fread(fid,1,'float64');
+info.NREF = fread(fid,1,'int');
+% ===================== starting and ending time  ======================= %
+% output as a string
+ctemp= findTblTag(fid,'STIM',1);
+info.STIM = [num2str(ctemp(17)),'-',num2str(ctemp(16)),'-','20',...
+    num2str(ctemp(18)),' ',num2str(ctemp(15)),':',num2str(ctemp(14)),...
+    ':',num2str(ctemp(13))];
+% output as a string
+ctemp = findTblTag(fid,'ETIM',1);
+info.ETIM = [num2str(ctemp(17)),'-',num2str(ctemp(16)),'-','20',...
+    num2str(ctemp(18)),' ',num2str(ctemp(15)),':',num2str(ctemp(14)),...
+    ':',num2str(ctemp(13))];
+% ========================= E and H channels  =========================== %
+findTblTag(fid,'EXLN',1);
+fseek(fid,-13,0);
+info.EXLN = fread(fid,1,'float64');
+findTblTag(fid,'EYLN',1);
+fseek(fid,-13,0);
+info.EYLN = fread(fid,1,'float64');
 % find_tbltag(fid,'EZLN',1);
 % fseek(fid,-13,0);
-ezln = fread(fid,1,'float32');
-find_tbltag(fid,'EGNC',1);
+% info.EZLN = fread(fid,1,'float64');
+ctemp = findTblTag(fid,'HXSN',1);
+info.HXSN = char(ctemp(13:24));
+ctemp = findTblTag(fid,'HYSN',1);
+info.HYSN = char(ctemp(13:24));
+ctemp = findTblTag(fid,'HZSN',1);
+info.HZSN = char(ctemp(13:24));
+findTblTag(fid,'EAZM',1);
 fseek(fid,-13,0);
-egnc = fread(fid,1,'int');
-find_tbltag(fid,'HGNC',1);
+info.EAZM = fread(fid,1,'float64');
+findTblTag(fid,'HAZM',1);
 fseek(fid,-13,0);
-hgnc = fread(fid,1,'int');
-find_tbltag(fid,'EGN',2);
+info.HAZM = fread(fid,1,'float64');
+% =================== L3, L4 and L5 sample parameter ==================== %
+findTblTag(fid,'HSMP',1);
+fseek(fid,-13,'cof');
+info.HSMP = fread(fid,1,'int');
+findTblTag(fid,'L3NS',1);
+fseek(fid,-13,'cof');
+info.L3NS = fread(fid,1,'int');
+findTblTag(fid,'L4NS',1);
+fseek(fid,-13,'cof');
+info.L4NS = fread(fid,1,'int');
+findTblTag(fid,'SRL3',1);
+fseek(fid,-13,'cof');
+info.SRL3 = fread(fid,1,'int');
+findTblTag(fid,'SRL4',1);
+fseek(fid,-13,'cof');
+info.SRL4 = fread(fid,1,'int');
+findTblTag(fid,'SRL5',1);
+fseek(fid,-13,'cof');
+info.SRL5 = fread(fid,1,'int');
+% ========================= gain and filtering ========================== %
+ctemp = findTblTag(fid,'LFRQ',1);
+info.LFRQ = ctemp(13:13);
+findTblTag(fid,'EGNC',1);
 fseek(fid,-13,0);
-egn = fread(fid,1,'int');
-find_tbltag(fid,'HGN',2);
+info.EGNC = fread(fid,1,'int');
+findTblTag(fid,'HGNC',1);
 fseek(fid,-13,0);
-hgn = fread(fid,1,'int');
-find_tbltag(fid,'SNUM',1);
+info.HGNC = fread(fid,1,'int');
+findTblTag(fid,'EGN',2);
 fseek(fid,-13,0);
-snum = fread(fid,1,'int');
-ctemp = find_tbltag(fid,'SITE',1);
-site = char(ctemp(13:16)');
-ctemp = find_tbltag(fid,'FILE',1);
-file = char(ctemp(13:20)');
-find_tbltag(fid,'EAZM',1);
+info.EGN = fread(fid,1,'int');
+findTblTag(fid,'HGN',2);
 fseek(fid,-13,0);
-eazm = fread(fid,1,'float');
-find_tbltag(fid,'HAZM',1);
+info.HGN = fread(fid,1,'int');
+findTblTag(fid,'HATT',1);
 fseek(fid,-13,0);
-hazm = fread(fid,1,'float');
-ctemp = find_tbltag(fid,'HXSN',1);
-hxsn = char(ctemp(13:20)');
-ctemp = find_tbltag(fid,'HYSN',1);
-hysn = char(ctemp(13:20)');
-ctemp = find_tbltag(fid,'HZSN',1);
-hzsn = char(ctemp(13:20)');
-find_tbltag(fid,'ELEV',1);
+info.HATT = fread(fid,1,'float64');
+findTblTag(fid,'HNOM',1);
 fseek(fid,-13,0);
-elev = fread(fid,1,'int');
-ctemp = find_tbltag(fid,'LATG',1);
-latg = char(ctemp(13:24)');
-ctemp = find_tbltag(fid,'LNGG',1);
-lngg = char(ctemp(13:24)');
+info.HNOM = fread(fid,1,'float64');
+findTblTag(fid,'TCMB',1);
+fseek(fid,-13,0);
+info.TCMB = fread(fid,1,'uint8');
+findTblTag(fid,'TALS',1);
+fseek(fid,-13,0);
+info.TALS = fread(fid,1,'uint8');
+findTblTag(fid,'LPFR',1);
+fseek(fid,-13,0);
+info.LPFR = fread(fid,1,'uint8');
+findTblTag(fid,'ACDC',1);
+fseek(fid,-13,0);
+info.ACDC = fread(fid,1,'uint8');
+findTblTag(fid,'FSCV',1);
+fseek(fid,-13,0);
+info.FSCV = fread(fid,1,'float64');
+% ======================================================================= %
+% now close the file
 fclose(fid);
-
-info.SNUM=snum;
-info.LAT=latg;
-info.LON=lngg;
-info.ELEV=elev;
-info.STIM=stim;
-info.ETIM=etim;
-info.EXLN=exln;
-info.EYLN=eyln;
-info.EZLN=ezln;
-info.EAZM=eazm;
-info.HAZM=hazm;
-info.HXSN=deblank(hxsn);
-info.HYSN=deblank(hysn);
-info.HZSN=deblank(hzsn);
-info.EGNC=egnc;
-info.EGN=egn;
-info.HGNC=hgnc;
-info.HGN=hgn;
-info.L3NS=l3ns;
-info.L4NS=l4ns;
-info.LFRQ=lfrq;
-info.HSMP=hsmp;
-info.SITE=site;
-info.FILE=file;
 return 
 
-function[ctemp,pos] = find_tbltag(fid,tag,inum)
-  % positions (pos) tbl file (fid) to certain tag (tag, ctemp)
-  % at its inum' occurrence in file.
-  fseek(fid,0,-1);
-  ctemp = fread(fid,25,'uint8');
-  n = 0;
-  while (~contains(char(ctemp'),tag) || n~=inum)
-      ctemp = fread(fid,25,'uint8');
-      if ( contains(char(ctemp'),tag))
-          n=n+1; 
-      end
-      if feof(fid) 
-          pos=0; 
-          disp(['Warning: not found: ',tag]);
-          return
-      end
-  end
-  pos = ftell(fid);
-  return
+function[ctemp,pos] = findTblTag(fid,tag,inum)
+% findTblTag find the position of a certain tag at its "inum"th occurrence 
+% in file - and returns the tag values, for the legacy Phoenix MTU-5A TBL 
+% format
+% 
+% ======================================================================= %
+% note:
+% yes, it's a bit silly to search for the tag each time from the beginning
+% it doesn't matter too much as the file is quite small
+% Hao
+% 2012.07.04
+% Beijing
+% ======================================================================= %
+% firstly return to the beginning of the file
+fseek(fid,0,-1);
+% read a first tag group
+ctemp = fread(fid,25,'uint8')';
+n = 0;
+% continue reading, until we got the "inum" occurance 
+while (~contains(char(ctemp),tag) || n~=inum)
+    % the size (tag + value) is always 25 bytes 
+    ctemp = fread(fid,25,'uint8')';
+    if (contains(char(ctemp),tag))
+        n=n+1; 
+    end
+    if feof(fid) 
+        pos=0; 
+        disp(['Warning: tag not found: ', tag]);
+        return
+    end
+end
+pos = ftell(fid);
+return
--- src/readTSN.m
+++ src/readTSN.m
@@ -1,23 +1,25 @@
-function [Ts, Tinfo]=readTSN(TSfid,DAC,ELN,csetup,Egain,Egainc,Hgain,Hgainc)
-% Nch=5 for Two E channels and Three H channels.
-% TSfid: the file handle of TS file
-% ELN:   1 by 3 array of Electrical Dipole length
-% csetup: channel setup to distinguish from e and h channels
-% Hgain: gain of H channels
-% Egain: gain of E channels
-% HNOM:  Normalizing Factor of H channels
-% DAC:   DA convert from recorded values to mV
+function [ts, tag]=readTSN(fpath,fname)
+% readTSN reads a (binary) TS file of the legacy Pheonix MTU-5A instrument 
+% (TS2, TS3, TS4, TS5) and the even older V5-2000 system (TSL, TSH), and 
+% output the "ts" array and "tinfo" metadata structure. 
 % ======================================================================= %
+% fpath: path to the TS file 
+% fname: name of the TS file (including extensions)
+% ts:    output array of the TS data
+% tag:   output tag struct of the TSn metadata
+% ======================================================================= %
+% defination of the TS tag (or what I guessed after reading the user manual
+% and fiddling with their files) 
 % 0-7   UTC time of first scan in the record. 
 % 8-9   instrument serial number (16-bit integer)
 % 10-11 number of scans in the record (16-bit integer)
 % 12    number of channels per scan
 % 13    tag length (TSn) or tag length code (TSH, TSL)
 % 14    status code
-% 15    bit-wise saturation flags (please note that the TSH/L tag ends
-% here )
+% 15    bit-wise saturation flags (please note that the older TSH/L tag 
+%       ends here )
 % 16    reserved for future indication of different tag and/or sample
-% formats
+%       formats
 % 17    sample length in bytes
 % 18-19 sample rate (in units defined by byte 20)
 % 20    units of sample rate
@@ -25,24 +27,29 @@ function [Ts, Tinfo]=readTSN(TSfid,DAC,ELN,csetup,Egain,Egainc,Hgain,Hgainc)
 % 22-25 clock error in seconds
 % 26-32 reserved; must be 0
 % ======================================================================= %
-% some notes on the ts format of tsn file
-% each time record consists of three bytes,  
-% let's name them byte 1, byte 2, and byte 3
-% the time record should be (+-) (byte2*256 + byte1)
-% please note that the third byte is merely a sigh byte. if read in uint8
-% format, it should be 255(-) or 0(+).
+% notes on the TS format of TSn files:
+% The binary TS file consists of several data blocks, each contains a data 
+% tag and a number of records in it.
+% Each time record consists of three bytes (24 bit), let's name them byte1,
+% byte2, and byte3:
+% the ts record (int) should be (+/-) (byte3*65536 + byte2*256 + byte1)
+%
+% Hao
+% 2012.07.04
+% Beijing
 % ======================================================================= %
 %  	try opening the ts data file 
-% p24=2^24;
-global custom
+TSfid=fopen([fpath fname ],'r','ieee-le');
+disp(['# opening file: ' fname]);
+% some constants used for format conversion
 p16=2^16;
 p8=2^8;
 % scan through time series
-% now reading a 32 Byte header info
+% firstly reading a 32 Byte header info
 s = fread(TSfid, 1, 'uint8' ) ; %Starting second
 if isempty(s)
-    Ts=[];
-    Tinfo=[];
+    ts=[];
+    tag=[];
     return
 end
 m = fread(TSfid, 1, 'uint8' ) ; %Starting minute
@@ -50,87 +57,79 @@ h = fread(TSfid, 1, 'uint8' ) ; %Starting hour
 d = fread(TSfid, 1, 'uint8' ) ; %Starting day
 l = fread(TSfid, 1, 'uint8' ) ; %Starting month
 y = fread(TSfid, 1, 'uint8' ) ; %Starting year
-fread(TSfid, 1, 'uint8' ) ; % skip the Starting weekday
+fread(TSfid, 1, 'uint8' ) ;     % skip the Starting weekday
 c = fread(TSfid, 1, 'uint8' ) ; %Starting centry(-1)
-bnum = fread ( TSfid, 1, 'uint16','ieee-le'); % box series number
+bnum = fread ( TSfid, 1, 'uint16'); % box series number
 Nscan = fread ( TSfid, 1, 'uint16'); % Number of scans in a data block
 Nch = fread ( TSfid, 1, 'uint8'); % Number of channels in a record
-% fread(TSfid,4,'uint32',0,'l'); % skip some (unknown) info...
-fseek(TSfid,5,'cof'); % skip to sampling frequency
-Fs = fread(TSfid, 1, 'uint16' ,0,'l'); % Sampling frequency
-fseek(TSfid,12,'cof');% skip some (unknown) head info...
-disp(['       sampling frequency is ' num2str(Fs) ' Hz'])
-disp(['       number of records is ' num2str(Nscan) ' in each data block'])
+Taglen = fread ( TSfid, 1, 'uint8'); % length of the tag
+if Taglen~=32
+    tstype='V5-2000';
+else
+    tstype='MTU-5';
+end
+disp(['# TS type is: ', tstype]);
+if Taglen==32
+    fseek(TSfid,4,'cof'); % skip to sampling frequency
+    Fs = fread(TSfid, 1, 'uint16' ,0,'l'); % Sampling frequency
+    fseek(TSfid,12,'cof');% skip some (unknown) head info...
+    disp(['# sampling frequency is ' num2str(Fs) ' Hz'])
+    disp(['# number of records is ' num2str(Nscan) ' in each data block'])
+end
 % now go to the end of the file...
 fseek(TSfid,0,'eof');
 Nblock=round(ftell(TSfid)/(Nscan*Nch*3+32)); % number of data blocks in the file
-Ts=zeros(Nch,Nscan*Nblock);
-disp(['       total ' num2str(Nblock) ' block(s) found in current file'])
-fseek(TSfid,0,'bof'); % now go to the beginning of the file...
-% multiply those together
-Egain = Egain * Egainc;
-if strfind(custom.method,'AMT')
-    Hgain = Hgain * Hgainc*1000;
-else
-    Hgain = Hgain * Hgainc;
-end
-
+% preallocate some memory for ts
+ts=zeros(Nch,Nscan*Nblock);
+disp(['# total ' num2str(Nblock) ' block(s) found in current file'])
+fseek(TSfid,0,'bof'); % now go back to the beginning of the file...
 for iblock=1:Nblock
     % now start loading the data 
     % here=ftell(TSfid); % for debug
     fseek(TSfid,32,'cof'); %skip the file tag
-    data=fread(TSfid,[Nch*3,Nscan],'uint8','ieee-le'); % 3*5  = 15 Byte per record
+    data=fread(TSfid,[Nch*3,Nscan],'uint8'); % 3*5  = 15 Byte per record
     if isempty(data)
-        disp('       finish reading time series...')
-        disp('       no data read in current block...')
-        fclose(TSfid);
+        disp('# warning: no data read in current block...')
         break
     end
     if Nch==3
-        Ts(csetup(1),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(3,:)*p16+data(2,:)*p8+data(1,:))*DAC/Egain/ELN(1)*1000; % Ex1 
-        Ts(csetup(2),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(6,:)*p16+data(5,:)*p8+data(4,:))*DAC/Egain/ELN(2)*1000; % Ex2
-        Ts(csetup(3),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(9,:)*p16+data(8,:)*p8+data(7,:))*DAC/Egain/ELN(3)*1000; % Ex3
+        ts(1,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(3,:)*p16+data(2,:)*p8+data(1,:)); % Ex1 
+        ts(2,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(6,:)*p16+data(5,:)*p8+data(4,:)); % Ex2
+        ts(3,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(9,:)*p16+data(8,:)*p8+data(7,:)); % Ex3
     elseif Nch==4
-        Ts(csetup(1),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(3,:)*p16+data(2,:)*p8+data(1,:))*DAC/Egain/ELN(1)*1000; % Ex1 
-        Ts(csetup(2),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(6,:)*p16+data(5,:)*p8+data(4,:))*DAC/Egain/ELN(2)*1000; % Ex2
-        Ts(csetup(3),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(9,:)*p16+data(8,:)*p8+data(7,:))*DAC/Egain/ELN(3)*1000; % Ex3
-        Ts(csetup(4),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(15,:)*p16+data(14,:)*p8+data(13,:))*DAC/Hgain; % Hy
+        ts(1,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(3,:)*p16+data(2,:)*p8+data(1,:)); % Ex1 
+        ts(2,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(6,:)*p16+data(5,:)*p8+data(4,:)); % Ex2
+        ts(3,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(9,:)*p16+data(8,:)*p8+data(7,:)); % Ex3
+        ts(4,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(15,:)*p16+data(14,:)*p8+data(13,:)); % Hy
     elseif Nch==5
-        Ts(csetup(1),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(3,:)*p16+data(2,:)*p8+data(1,:))*DAC/Egain/ELN(1)*1000; % mV/km
-        Ts(csetup(2),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(6,:)*p16+data(5,:)*p8+data(4,:))*DAC/Egain/ELN(2)*1000; % mV/km
-        Ts(csetup(3),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(9,:)*p16+data(8,:)*p8+data(7,:))*DAC/Hgain; % Hx nT
-        Ts(csetup(4),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(12,:)*p16+data(11,:)*p8+data(10,:))*DAC/Hgain; % Hy nT
-        Ts(csetup(5),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(15,:)*p16+data(14,:)*p8+data(13,:))*DAC/Hgain; % Hz nT
+        ts(1,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(3,:)*p16+data(2,:)*p8+data(1,:)); % Ex
+        ts(2,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(6,:)*p16+data(5,:)*p8+data(4,:)); % Ey
+        ts(3,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(9,:)*p16+data(8,:)*p8+data(7,:)); % Hx 
+        ts(4,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(12,:)*p16+data(11,:)*p8+data(10,:)); % Hy 
+        ts(5,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(15,:)*p16+data(14,:)*p8+data(13,:)); % Hz 
     elseif Nch==6
-        Ts(csetup(1),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(3,:)*p16+data(2,:)*p8+data(1,:))*DAC/Egain/ELN(1)*1000; % Ex mV/km
-        Ts(csetup(2),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(6,:)*p16+data(5,:)*p8+data(4,:))*DAC/Egain/ELN(2)*1000; % Ey mV/km
-        Ts(csetup(3),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(9,:)*p16+data(8,:)*p8+data(7,:))*DAC/Egain/ELN(3)*1000; % Ez mV/km
-        Ts(csetup(4),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(12,:)*p16+data(11,:)*p8+data(10,:))*DAC/Hgain; % Hx nT
-        Ts(csetup(5),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(15,:)*p16+data(14,:)*p8+data(13,:))*DAC/Hgain; % Hy nT
-        Ts(csetup(6),(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(18,:)*p16+data(17,:)*p8+data(16,:))*DAC/Hgain; % Hz nT
+        ts(1,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(3,:)*p16+data(2,:)*p8+data(1,:)); % Ex 
+        ts(2,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(6,:)*p16+data(5,:)*p8+data(4,:)); % Ey 
+        ts(3,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(9,:)*p16+data(8,:)*p8+data(7,:)); % Ez 
+        ts(4,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(12,:)*p16+data(11,:)*p8+data(10,:)); % Hx 
+        ts(5,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(15,:)*p16+data(14,:)*p8+data(13,:)); % Hy 
+        ts(6,(iblock-1)*Nscan+1:iblock*Nscan)=getsign24(data(18,:)*p16+data(17,:)*p8+data(16,:)); % Hz 
     end    
 end
-% now we consider trimming the time series to remove those we don't need
-tskip1 = custom.tskip1;
-tskip2 = custom.tskip2;
-if (tskip1 > 0) && (tskip1 < 100)
-    scan1 = ceil(Nblock*tskip1/100) + 1;
-else
-    scan1 = 1;
-end
-if (tskip2 > 0) && (tskip2 < 100)
-    scan2 = Nblock - ceil(Nblock*tskip2/100);
-else
-    scan2 = Nblock;
-end
-if scan2 - scan1 > Nblock * 0.32
-    Ts = Ts(:,scan1*Nscan:scan2*Nscan);
-end
-Tinfo.bnum=bnum;
-Tinfo.Fs=Fs;
-Tinfo.Nch=Nch;
-Tinfo.Nscan=Nscan;
-Tinfo.Tstr=[(c*100)+y l d h m s];
-Tinfo.Tlen=Nscan/Fs;
-Tinfo.Nblock=Nblock;
+fclose(TSfid);
+disp('# finish reading time series...')
+tag.boxnum=bnum;
+tag.tstype=tstype;
+tag.Fs=Fs;
+tag.Nch=Nch;
+tag.Nscan=Nscan;
+tag.Tstr=[(c*100)+y l d h m s];
+tag.Tlen=Nscan/Fs;
+tag.Nblock=Nblock;
+return
+
+function x=getsign24(x)
+% a simple function to calculate the sign for a 24 bit number
+% I should have made it in-line
+x(x>2^23-1)=x(x>2^23-1)-2^24;
 return
