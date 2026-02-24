164171449ff352745fc61765d245e49280c9bd94
DONG Hao <donghao@cugb.edu.cn>
2026-02-24 15:37:26 +0800

    Enhance documentation and add calibration functions
    Updated module documentation and added new imports for box and coil calibration functions. Also modified the __all__ and __version__ attributes.

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,11 @@
 """
-MTU_utils - Utility to read the Canadian Phoenix MTU-5A instrument
-time series binary files in Matlab (and Python)
+MTU_utils - Utility to read the Canadian Phoenix MTU-5A instrument 
+files (time series and instrument response ) in Matlab (and Python)
 
-A bunch of simple scripts to read the legacy Phoenix MTU-5A binary format
-files ... including the time series (.TSN) and table (.TBL) formats.
+A bunch of simple scripts to read the legacy Phoenix MTU-5A format
+files ... including the time series (.TSN) and table (.TBL) formats, 
+and the instrument (.clb) and coil (.clc) calibration ascii formats.
+
 
 DONG Hao
 donghao@cugb.edu.cn
@@ -12,6 +14,8 @@ China University of Geosciences, Beijing
 
 from .read_tbl import read_tbl
 from .read_tsn import read_tsn
+from .read_boxcal import read_boxcal
+from .read_coilcal import read_coilcal
 
-__all__ = ['read_tbl', 'read_tsn']
-__version__ = '1.0.0'
+__all__ = ['read_tbl', 'read_tsn', "read_boxcal", "read_coilcal"]
+__version__ = '1.0.1'
